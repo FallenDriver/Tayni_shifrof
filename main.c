dd5251f5f6fc65bc63d892c405618fae40438bf3
Fallen_Driver <agen14syes@gmail.com>
2026-05-18 18:41:36 +0700

    added function generate_key

--- main.c
+++ main.c
@@ -4,7 +4,33 @@
 #include <openssl/ec.h>
 
 
+EVP_PKEY* generate_key() {
 
+    EVP_PKEY_CTX *ctx;
+    ctx = EVP_PKEY_CTX_new_from_name(NULL, "EC", NULL);
+
+    if (EVP_PKEY_keygen_init(ctx) <= 0) {
+        printf("Ошибка генерации ключа\n");
+        EVP_PKEY_CTX_free(ctx);
+        return NULL;
+    }
+
+    if (EVP_PKEY_CTX_set_group_name(ctx, "secp256k1") == 0) {
+        printf("Ошибка генерации\n");
+        EVP_PKEY_CTX_free(ctx);
+        return NULL;
+    }
+
+    EVP_PKEY *pkey = NULL;
+    if (EVP_PKEY_keygen(ctx, &pkey) <= 0) {
+        printf("Ошибка генерации");
+        EVP_PKEY_CTX_free(ctx);
+        return NULL;
+    }
+
+    EVP_PKEY_CTX_free(ctx);
+    return pkey;
+}
 
 
 int main() {
@@ -16,25 +42,16 @@ int main() {
 
     printf("Версия OpenSSL: %s\n", OpenSSL_version(OPENSSL_VERSION));
 
-    EVP_PKEY_CTX *ctx;
-
-    ctx = EVP_PKEY_CTX_new_from_name(NULL, "EC", NULL);
-    if (EVP_PKEY_keygen_init(ctx) <= 0) {
-        printf("Ошибка генерации ключа\n");
-        return 1;
-    }
-
-    if (EVP_PKEY_CTX_set_group_name(ctx, "secp256k1") == 0) {
-        printf("Ошибка генерации\n");
-        return 1;
-    }
-
-    EVP_PKEY *pkey = NULL;
-    EVP_PKEY_keygen(ctx, &pkey);
     
+    EVP_PKEY *mikhail_key = generate_key();
+    EVP_PKEY *alexandra_key = generate_key();
+
+    if (mikhail_key != NULL && alexandra_key != NULL) {
+        printf("Ключи сгенерированы успешно!\n");
+    }
     
-    EVP_PKEY_CTX_free(ctx);
-    EVP_PKEY_free(pkey);
+    EVP_PKEY_free(mikhail_key);
+    EVP_PKEY_free(alexandra_key);
 
     OPENSSL_cleanup();
     return 0;
