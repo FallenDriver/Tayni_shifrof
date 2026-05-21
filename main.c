16e91dfa92bbf0e3d6ca81b94ea06be4a08aca12
Fallen_Driver <agen14syes@gmail.com>
2026-05-21 22:04:03 +0700

    added secret_generation function

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <openssl/evp.h>
 #include <openssl/err.h>
 #include <openssl/ec.h>
@@ -23,7 +25,7 @@ EVP_PKEY* generate_key() {
 
     EVP_PKEY *pkey = NULL;
     if (EVP_PKEY_keygen(ctx, &pkey) <= 0) {
-        printf("Ошибка генерации");
+        printf("Ошибка генерации\n");
         EVP_PKEY_CTX_free(ctx);
         return NULL;
     }
@@ -32,6 +34,27 @@ EVP_PKEY* generate_key() {
     return pkey;
 }
 
+unsigned char *secret_generation(EVP_PKEY *private_key, EVP_PKEY *public_key, int *len_result) {
+    EVP_PKEY_CTX *ctx;
+    ctx = EVP_PKEY_CTX_new_from_pkey(NULL, private_key, NULL);
+
+    size_t len = 0;
+
+    EVP_PKEY_derive_init(ctx);
+
+    EVP_PKEY_derive_set_peer(ctx, public_key);
+
+    EVP_PKEY_derive(ctx, NULL, &len);
+
+    unsigned char *secret = malloc(len);
+
+    EVP_PKEY_derive(ctx, secret, &len);
+
+    *len_result = len;
+    EVP_PKEY_CTX_free(ctx);
+
+    return secret;
+}
 
 int main() {
     
@@ -49,9 +72,25 @@ int main() {
     if (mikhail_key != NULL && alexandra_key != NULL) {
         printf("Ключи сгенерированы успешно!\n");
     }
+
+    int m_len, a_len;
+
+    unsigned char *mikhail_secret = secret_generation(mikhail_key, alexandra_key, &m_len);
+    unsigned char *alexandra_secret = secret_generation(alexandra_key, mikhail_key, &a_len);
+
+
+    if (m_len == a_len && memcmp(mikhail_secret, alexandra_secret, m_len) == 0) {
+        printf("Секреты совпадают!\n");
+    } else {
+        printf("Ошибка: секреты не совпадают!\n");
+    }
+
+
     
     EVP_PKEY_free(mikhail_key);
     EVP_PKEY_free(alexandra_key);
+    free(mikhail_secret);
+    free(alexandra_secret);
 
     OPENSSL_cleanup();
     return 0;
